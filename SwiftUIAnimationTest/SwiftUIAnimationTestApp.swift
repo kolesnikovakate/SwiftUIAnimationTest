1a5ab741bba3e8fc4888c50249e73f4a0c1a6bfb
kolesnikovakate <kolesnikovakate@gmail.com>
2023-05-24 22:09:07 +0200

    Initial Commit

---
+++ SwiftUIAnimationTest/SwiftUIAnimationTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAnimationTestApp.swift
+//  SwiftUIAnimationTest
+//
+//  Created by Ekaterina Kolesnikova on 5/24/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAnimationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
